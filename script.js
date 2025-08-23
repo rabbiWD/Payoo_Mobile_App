dec502a32ea094667a5a965c99c91e88461011ca
Rabbi Khan <Krabby2244@gmail.com>
2025-08-23 22:49:13 +0600

    login button functioality almost done

---
+++ script.js
@@ -0,0 +1,22 @@
+// login button functionality
+document.getElementById("loginButton").addEventListener("click",function(e){
+    e.preventDefault()
+    // console.log("login button clicked");
+    const mobileNumber = 12345678920
+    const pinNumber = 2234
+
+    const mobileNumberValue = parseInt( document.getElementById("mobile-number").value)
+
+    // const mobileNumberValue = document.getElementById("mobile-number").value
+    // const mobileNumberValueConverted = parseInt(mobileNumberValue)
+    const pinNumberValue = parseInt(document.getElementById("pin-number").value)
+    // const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    console.log(mobileNumberValue, pinNumberValue);
+
+    if(mobileNumberValue === mobileNumber && pinNumberValue === pinNumber){
+        console.log("all value match");
+    }else{
+        console.log("invalid credentials");
+    }
+})
