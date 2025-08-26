e61a9b93fd79f57a5704059f63fea8a571f6cd06
Rabbi Khan <Krabby2244@gmail.com>
2025-08-27 00:38:10 +0600

    home.js add money feature  & cashout feature almost done

--- home.js
+++ home.js
@@ -1,16 +1,53 @@
-
+validPin =1234
+// add money feature
 document.getElementById("add-money-btn").addEventListener("click",function(e){
     e.preventDefault()
     // console.log("add money btn");
     const bank = document.getElementById("bank").value
     const accountNumber = document.getElementById("account-number").value
     const amount =parseInt(document.getElementById("add-amount").value)
-    const pin = document.getElementById("add-pin").value
+    const pin = parseInt(document.getElementById("add-pin").value)
     // console.log(bank,accountNumber,amount,pin);
     const availableBalance = parseInt(document.getElementById("available-balance").innerText)
-    console.log(availableBalance);
+    console.log(amount, availableBalance);
+
+    if(accountNumber.length <11){
+        alert("please provide valid account number")
+        return;
+    }
+
+    if(pin !== validPin){
+        alert("please provide valid pin number")
+        return;
+    }
+
     const totalNewAvailableBalance = amount + availableBalance
     // console.log(totalNewAvailableBalance);
     
     document.getElementById("available-balance").innerText = totalNewAvailableBalance
+})
+
+// cash out money feature
+document.getElementById("withdraw-btn").addEventListener("click", function(e){
+    e.preventDefault()
+    // console.log("withdraw btn click");
+    const amount = parseInt(document.getElementById("withdraw-amount").value)
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+    // console.log(amount, availableBalance);
+    const totalNewAvailableBalance = availableBalance - amount
+    // console.log(totalNewAvailableBalance);
+    document.getElementById("available-balance").innerText = totalNewAvailableBalance
+})
+
+
+
+// toggling feature 
+document.getElementById("add-button").addEventListener("click", function(e){
+    document.getElementById("cash-out-parent").style.display = "none"
+    document.getElementById("add-money-parent").style.display = "block"
+})
+
+document.getElementById("cash-out-button").addEventListener("click", function(e){
+    document.getElementById("add-money-parent").style.display = "none"
+    document.getElementById("cash-out-parent").style.display = "block"
 })
