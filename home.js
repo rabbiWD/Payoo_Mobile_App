c738c46427470d1dae7ab6fbb24b27f8ef771578
Rabbi Khan <Krabby2244@gmail.com>
2025-08-26 20:45:43 +0600

    home.js almost done

--- home.js
+++ home.js
@@ -1,1 +1,16 @@
-console.log("home js connected");
+
+document.getElementById("add-money-btn").addEventListener("click",function(e){
+    e.preventDefault()
+    // console.log("add money btn");
+    const bank = document.getElementById("bank").value
+    const accountNumber = document.getElementById("account-number").value
+    const amount =parseInt(document.getElementById("add-amount").value)
+    const pin = document.getElementById("add-pin").value
+    // console.log(bank,accountNumber,amount,pin);
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+    console.log(availableBalance);
+    const totalNewAvailableBalance = amount + availableBalance
+    // console.log(totalNewAvailableBalance);
+    
+    document.getElementById("available-balance").innerText = totalNewAvailableBalance
+})
